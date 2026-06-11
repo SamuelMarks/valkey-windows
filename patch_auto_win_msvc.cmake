6cbc5cc4de5f6ec162e778ae457f03467fc5f81b
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2026-06-11 22:32:12 +1200

    Fix patch application for 9.1.0

--- patch_auto_win_msvc.cmake
+++ patch_auto_win_msvc.cmake
@@ -21,3 +21,8 @@ file(READ "valkey/deps/libvalkey/src/net.c" CONTENT)
 string(REPLACE "if (ioctl(c->fd, FIONBIO, &mode) == -1) {" "if (posix_ioctl(c->fd, FIONBIO, &mode) == -1) {" CONTENT "${CONTENT}")
 string(REPLACE "#include \"valkey_private.h\"" "#include \"valkey_private.h\"\n#undef ioctl\n#define ioctl posix_ioctl\n#undef socket\n#define socket posix_socket\n#undef poll\n#define poll posix_poll\n#undef getsockopt\n#define getsockopt posix_getsockopt\n#undef setsockopt\n#define setsockopt posix_setsockopt\n#undef bind\n#define bind posix_bind\n#undef listen\n#define listen posix_listen\n#undef accept\n#define accept posix_accept\n#undef recv\n#define recv posix_recv\n#undef send\n#define send posix_send\n#undef freeaddrinfo\n#define freeaddrinfo posix_freeaddrinfo\n#undef getaddrinfo\n#define getaddrinfo posix_getaddrinfo\n#undef gai_strerror\n#define gai_strerror posix_gai_strerror\n#undef connect\n#define connect posix_connect\n#undef close\n#define close posix_close\n" CONTENT "${CONTENT}")
 file(WRITE "valkey/deps/libvalkey/src/net.c" "${CONTENT}")
+
+file(READ "patches/0001-Windows-native-builds.patch" PATCH_CONTENT)
+string(REPLACE "\n\n@@" "\n \n@@" PATCH_CONTENT "${PATCH_CONTENT}")
+string(REPLACE "\r\n\r\n@@" "\r\n \r\n@@" PATCH_CONTENT "${PATCH_CONTENT}")
+file(WRITE "patches/0001-Windows-native-builds.patch" "${PATCH_CONTENT}")
